b8f9befd845c898003fa4f46e35cfd7803ba2440
seintax89 <davidfego22@gmail.com>
2026-04-19 21:52:20 -0500

    Corregir API y CORS para la Produccion

--- frontend-medico/src/services/api.js
+++ frontend-medico/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://portal-medico-uicj.onrender.com',
+    baseURL: 'https://portal-medico-uicj.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
